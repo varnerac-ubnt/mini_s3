89099e4c2fed6cbde7f892b76b8d0d4d1476baae
varnerac-ubnt <drew.varner@ubnt.com>
2014-07-13 18:44:29 -0500

    This is an experimental branch of mini_s3.
    * The code is heavily refactored. It uses 80 characters per line.
    * There are Makefile changes
    * It contains an initial integtation of `fakes3` to suppor integration testing
      without using a real Amazon S3 account.
    * It uses ferd's `lhttpc` fork optimized for multiple connections to the same
      site instead of `ibrowse`

---
+++ test/fakes3_server.erl
@@ -0,0 +1,72 @@
+-module(fakes3_server).
+
+-export([start_fakes3/1]).
+
+-behaviour(gen_server).
+
+% API
+-export([start/1]).
+-export([stop/0]).
+
+% gen_server callbacks
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([terminate/2]).
+-export([handle_info/2]).
+-export([code_change/3]).
+
+%%
+%% API
+%%
+
+-spec start(Cmd::string()) -> {ok, pid()}.
+start(Cmd) ->
+    gen_server:start({local, ?MODULE}, ?MODULE, Cmd, []).
+
+-spec stop() -> ok.
+stop() ->
+    gen_server:call(?MODULE, stop).
+
+%%
+%% gen_server callbacks
+%%
+
+init(Cmd) ->
+    Port = start_fakes3(Cmd),
+    {ok, Port}.
+
+handle_call(stop, _From, {port, Port, pid, OSPid}) ->
+    io:format("~p is shutting down port: ~p~n", [?MODULE, Port]),
+    ok = stop_fakes3({port, Port, pid, OSPid}),
+    {stop, normal, ok, {port, Port, pid, OSPid}}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+handle_cast(_, State) ->
+    {noreply, State}.
+
+terminate(_Reason, State) ->
+    {ok, State}.
+
+code_change(_, State, _) ->
+    {ok, State}.
+
+%%
+%% Private 
+%%
+
+-spec start_fakes3(Cmd::string()) -> port().
+start_fakes3(Cmd) ->
+    Port = open_port({spawn, Cmd}, []),
+    {os_pid, OSPid} = erlang:port_info(Port, os_pid),
+    {port, Port, pid, OSPid}.
+
+stop_fakes3({port, Port, pid, OSPid}) ->
+    port_close(Port),
+    case os:type() of 
+        {unix, _}  -> os:cmd(io_lib:format("kill -9 ~p", [OSPid]));
+        {win32, _} -> os:cmd(io_lib:format("taskkill /PID ~p", [OSPid]))
+    end,
+    ok.
